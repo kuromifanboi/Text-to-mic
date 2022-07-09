ebd65a11ff979728711d454b73349b524085ddf0
Tom Do <tom@iifuture.com>
2022-07-09 15:05:55 +1000

    Added the rest of the code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from multiprocessing.connection import wait
 import pyttsx3
 
@@ -10,4 +11,32 @@ while True:
     engine.runAndWait()
     engine.save_to_file(text, 'text.mp3')
     engine.runAndWait()
+=======
+from multiprocessing.connection import wait
+import pyttsx3
+import time
+from pygame import mixer
+
+
+while True:
+    #get the text needed and convert it to mp3
+    text = input('Enter your text: ')
+    print('saying: ' + text)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.save_to_file(text, 'text.mp3')
+    engine.runAndWait()
+    print('\n')
+
+
+    #play the mp3 over the microphone
+    mixer.init()
+    mixer.music.load('text.mp3')
+    mixer.music.play()
+    time.sleep(5)
+    mixer.music.stop()
+    mixer.quit()
+    time.sleep(1)
+>>>>>>> 0ef9e81 (Added the rest of the code)
     print('\n')
