8f0859f1d6bf4e9f3fc63350dbbe143ef789a2c1
Tom Do <tom@iifuture.com>
2022-07-09 15:30:29 +1000

    Progress

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from multiprocessing.connection import wait
 import pyttsx3
 import time
-from pygame import mixer
-
+from pygame import mixer, _sdl2
+import sounddevice as sd
 
 while True:
     #get the text needed and convert it to mp3
     text = input('Enter your text: ')
     print('saying: ' + text)
     engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
     engine.save_to_file(text, 'text.mp3')
     engine.runAndWait()
     print('\n')
@@ -18,10 +16,6 @@ while True:
 
     #play the mp3 over the microphone
     mixer.init()
-    mixer.music.load('text.mp3')
-    mixer.music.play()
-    time.sleep(5)
-    mixer.music.stop()
-    mixer.quit()
-    time.sleep(1)
-    print('\n')
+
+    print("Inputs:", _sdl2.audio.get_audio_device_names(True))
+    print("Outputs:", _sdl2.audio.get_audio_device_names(False))
